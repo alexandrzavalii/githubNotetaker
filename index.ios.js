81357f368aca330daba2bc9e5b6ceaf7b11f91dd
alexandrzavalii <alexandrzavalii@gmail.com>
2017-05-20 23:44:18 +0100

    Testing Route

--- index.ios.js
+++ index.ios.js
@@ -5,48 +5,33 @@
  */
 
 import React, { Component } from 'react';
+import Main from './App/Components/Main.js';
+
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS
 } from 'react-native';
 
 export default class githubNotetaker extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+          style={styles.container}
+          initialRoute={{
+            title: 'Github Notetaker',
+            component: Main
+          }} />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#111111'
   },
 });
 
